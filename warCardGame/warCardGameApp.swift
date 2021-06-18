eb7c8067fa02e8aff78f43fd2936dcfbbe9b9d9d
Mitchman215 <85707581+Mitchman215@users.noreply.github.com>
2021-06-18 14:27:03 -0400

    Initial Commit

---
+++ warCardGame/warCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  warCardGameApp.swift
+//  warCardGame
+//
+//  Created by Mitchell Salomon on 6/18/21.
+//
+
+import SwiftUI
+
+@main
+struct warCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
